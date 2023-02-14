82a8a82ca1b5a943a20ea016d2ea9e41bc600af8
Xilong <2875319753@qq.com>
2023-02-14 11:09:43 +0800

    第一次提交

---
+++ git_demo01/src/main/java/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: GitTest
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author 张禧龙
+ * @Create 2023-02-14 11:08
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
